48f8b2675031772d7175c8bb4d0aa8ef488c75a9
AllanMD <allanavatar@hotmail.com>
2020-01-10 17:12:45 -0300

    login with token finished

--- login-with-token/src/app/routes/users.js
+++ login-with-token/src/app/routes/users.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 const dbConnection = require("../../config/dbConnection"); // para conexion a bd // por cada "../" sube un nivel en las carpetas
 const redis = require("../../config/redis"); // para conexion a redis
-redis(); //llama a la funcion del REDIS
-var tokens = "";
 
 class User {
     constructor(user, pass) {
@@ -19,32 +17,30 @@ let response = {
     message: ''
 };
 
-/** 
- * Para entender el next, probar y comentar la linea next();
- */
-var myLogger = function (req, res, next) {
-    console.log('Logger');
-    next(); // pasa el control al endpoint que fue llamado en primer lugar
-};
-
-router.use(myLogger); // router.use va a usar el middleware pasado por parametro en cada llamada a este route
+//router.use(middleware); // router.use va a usar el middleware pasado por parametro en cada llamada a este route
 
 /**
  * Funcion que autentica mediante un token una llamada a una api
  * https://carlosazaustre.es/autenticacion-con-token-en-node-js/
  */
 function auth(req, res, next) {
-    console.log("Headerss: " + req.headers.host);
     if (!req.headers.authorization) {
         return res.status(401).send({ message: "La peticion no tiene header de autorizacion" });
     }
-    else if (req.headers.authorization !== tokens) { //TODO: cambiar a una funcion validateToken, q lo va a buscar en el bd redis
-        return res.status(403).send({ message: "Token incorrecto!" });
-    }
-    next();
-    // en caso de que todo este bien, con el next() se pasa el control a la siguiente funcion middleware
+    redis.exists(req.headers.authorization, function (err, reply) {
+        if (err) {
+            console.log("error: " + err);
+            return res.status(500).send({ message: "Error con la base de datos!" });
+        }
+        if (reply === 0) { // 0 = no existe, 1 = si existe
+            console.log("TOKEN INCORRECTO");
+            return res.status(403).send({ message: "El token expiró o es incorrecto!" });
+        }
+        next();
+        // en caso de que todo este bien, con el next() se pasa el control a la siguiente funcion middleware
 
-    //esto puede ir en un archivo separado y ser exportado, como aparece ne la pagina
+        //esto puede ir en un archivo separado y ser exportado, como aparece en la pagina
+    });
 };
 
 
@@ -52,13 +48,12 @@ function auth(req, res, next) {
  * Para agrupar todos los metodos que esten bajo una misma ruta
  */
 router.route('/')
-    // el primer middleware ejecuta la autenticacion, si sale bien, continua con el siguiente
+    // el primer middleware ejecuta la autenticacion, si sale bien, continua con el siguiente middleware
     .get(auth, function (req, res) {
         const connection = dbConnection();
         connection.query('SELECT * FROM users', function (error, results, fields) {
             if (error) {
-                res.status(500).send("Error obteniendo los usuarios");
-                throw error;
+                res.status(500).send("Error obteniendo los usuarios de la bd");
             } else {
                 res.status(200).send(results);
             }
@@ -87,7 +82,11 @@ router.route('/')
                             message: "Usuario ya existe"
                         };
                     } else {
-                        throw error;
+                        response = {
+                            error: true,
+                            code: 500,
+                            message: error
+                        };
                     }
                     console.log("Error code: " + error.code);
                 } else {
@@ -139,12 +138,16 @@ router.route('/')
     })*/
     ;
 
-router.delete("/:id", auth, function (req, res) {
+router.delete("delete/:id", auth, function (req, res) {
     const connection = dbConnection();
     connection.query('DELETE FROM users WHERE user_id = ?', [req.params.id], function (error, result) {
         if (error) {
+            response = {
+                error: true,
+                code: 500,
+                message: error
+            }
             console.log("Code:" + error.code);
-            throw error;
         } else {
             if (result.affectedRows == 0) {
                 response = {
@@ -170,8 +173,12 @@ router.post("/login", function (req, res) {
     const connection = dbConnection();
     connection.query('SELECT * FROM users WHERE user = ?', [req.body.user], function (error, results, fields) {
         if (error) {
+            response = {
+                error: true,
+                code: 500,
+                message: error
+            };
             console.log(error.code);
-            throw error;
         } else {
             if (results.length == 0) {
                 response = {
@@ -179,48 +186,60 @@ router.post("/login", function (req, res) {
                     code: 401,
                     message: "Usuario no existe"
                 };
-                res.status(response.code).send(response);
             } else if (results[0].pass != req.body.pass) {
                 response = {
                     error: true,
                     code: 401,
                     message: "Contraseña incorrecta"
                 }
-                res.status(response.code).send(response)
             } else {
-                tokens = generateToken();
+                let token = generateToken();
                 response = {
                     error: false,
                     code: 200,
                     message: "Login exitoso!",
-                    token: tokens
-                }
-                res.status(response.code).send(response);
+                    token: token
+                };
+                redis.set(token, req.body.user);
             }
         }
+        res.status(response.code).send(response);
     });
     connection.end();
 });
 
+/**
+ * Metodo para desloguearse. Elimina el token del redis.
+ */
+router.delete("/logout", auth, function (req, res) {
+    redis.delete(req.headers.authorization, function (error, reply) {
+        if (error) {
+            response = {
+                error: true,
+                code: 500,
+                message: error
+            }
+            console.log(error);
+        }
+        else {
+            response = {
+                error: false,
+                code: 200,
+                message: "Logout exitoso!"
+            }
+        }
+        res.status(response.code).send(response);
+    });
+})
+
+/**
+ * Genera un token. El token es un string aleatorio.
+ */
 function generateToken() { //https://unipython.com/generar-token-random-con-java-script/
     var token = Math.random().toString(36).substring(2);
     console.log("Token: " + token);
     return token;
 }
 
-function validateToken(token){
-    const redisClient = redis();
-    redisClient.exists(token, function (err, reply) {
-        if (err != null) {
-            //error
-            console.log("halo")
-        }
-        else {
-            console.log("sfa");
-        }
-    });
-}
-
 module.exports = router;
 
-//TODO: documentar codigo
--- login-with-token/src/config/redis.js
+++ login-with-token/src/config/redis.js
@@ -1,32 +1,75 @@
 //incluimos redis a nuestro script
 var redis = require('redis');
 
-module.exports = () => {
-    //creamos un cliente
+// para entender module.exports : https://www.sitepoint.com/understanding-module-exports-exports-node-js/
+const getConnection = () => {
     var redisClient = redis.createClient();
 
     redisClient.on('connect', function () {
         console.log('Conectado a Redis Server');
     });
 
-    redisClient.set("key1", "hola mundo1");
-    redisClient.set("key2", "hola mundo2");
-    redisClient.set("key3", "hola mundo2");
-    redisClient.get("key1", function (err, value) {
-        // retornara null si la key no existe
-        console.log(value);
-    });
-
-    redisClient.exists("key1", function (err, reply) {
-        if (err != null) {
-            //error
-            console.log("halo")
-        }
-        else {
-            console.log("sfa");
-        }
-    });
-    //TODO: borrar los ejemplos
-    //  redisClient.set(["key1", "val1","val2","val3","val4"]); //Tomara el primer elemento del array y lo usara como un identificar y los demás elementos serán los valores.
-
+    return redisClient;
 }
+
+exports.set = function (key, value) {
+    let redisClient = getConnection();
+    redisClient.set(key, value);
+    redisClient.expire(key, 120); //https://redis.io/commands/expire // el tiempo es en segundos
+    redisClient.quit();
+}
+
+exports.get = function (key, callback) {
+    let redisClient = getConnection();
+    redisClient.get(key, function (err, value) {
+        if (err) {
+            return callback(err);
+        } else {
+            return callback(null, value);
+        }
+    });
+    redisClient.quit();
+}
+
+exports.getAllKeys = function (callback) {
+    let redisClient = getConnection();
+    redisClient.keys("'*'", function (err, value) {
+        if (err) {
+            console.log("Error");
+            return callback(err);
+        } else {
+            console.log("value");
+            return callback(null, value);
+        }
+    })
+    redisClient.quit();
+}
+
+exports.exists = function (key, callback) {
+    let redisClient = getConnection();
+    redisClient.exists(key, function (err, reply) {
+        if (err) {
+            console.log("Error: " + err);
+            return callback(err);
+        } else {
+            return callback(null, reply);
+        }
+    });
+    redisClient.quit();
+}
+
+exports.delete = function (key, callback) {
+    let redisClient = getConnection();
+    redisClient.del(key, function (err, reply) {
+        if (err) {
+            console.log("Error: " + err);
+            return callback(err);
+        } else {
+            return callback(null, reply);
+        }
+    });
+    redisClient.quit();
+}
+
+// redisClient.set(["key1", "val1","val2","val3","val4"]); //Tomara el primer elemento del array y lo usara como un identificar y los demás elementos serán los valores.
+// en redis-cli,el comando: keys '*' sirve para ver todas las keys guardadas.
